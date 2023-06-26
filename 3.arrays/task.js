219c243c4b3840dd969bb57235506ba259a7ebb9
killakode <sudarev.n@yandex.ru>
2023-06-26 04:06:32 +0300

    Частично выполнено 3 задание

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,8 @@
 function compareArrays(arr1, arr2) {
-  
+  return arr1.length === arr2.length && arr1.every((element, idx) => element === arr2[idx]); 
 }
 
 function getUsersNamesInAgeRange(users, gender) {
-  
+
+  let result = user.filter( e => e.gender === "мужской").map((x,idx) => x += user.age[idx]).reduce((acc,ageAll) => (acc + ageAll) / users.length, 0);
 }
