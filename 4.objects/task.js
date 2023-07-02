a6808959a0495d8672b74bf44bc6ddf6b6e1714f
killakode <sudarev.n@yandex.ru>
2023-07-02 14:46:16 +0300

    4 выполненое задание

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,30 @@
 function Student(name, gender, age) {
-  
+    this.marks = [];
+    this.name = name;
+    this.gender = gender;
+    this.age = age;
 }
 
-Student.prototype.setSubject = function (subjectName) {
-  
+
+Student.prototype.setSubject = function(subjectName) {
+    this.subject = subjectName;
 }
 
-Student.prototype.addMarks = function (...marks) {
-  
+Student.prototype.addMarks = function(...marks) {
+    if (this.hasOwnProperty(`marks`) === true) {
+        this.marks.push(...marks);
+    }
 }
 
-Student.prototype.getAverage = function () {
-  
+Student.prototype.getAverage = function() {
+    if (this.hasOwnProperty(`marks`) === false || this.marks.length === 0) {
+        return 0;
+    }
+    return this.marks.reduce((acc, item) => acc += item, 0) / this.marks.length;
 }
 
-Student.prototype.exclude = function (reason) {
-  
+Student.prototype.exclude = function(reason) {
+    delete this.subject;
+    delete this.marks;
+    this.excluded = reason;
 }
