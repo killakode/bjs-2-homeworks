e8a99273e34c96151dd27fe54b798c4a4ea66589
killakode <sudarev.n@yandex.ru>
2023-07-11 16:58:39 +0300

    Сделал 8 задание

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -1,9 +1,48 @@
 //Задача № 1
 function cachingDecoratorNew(func) {
-  
+    let cache = [];
+
+    function wrapper(...args) {
+        const hash = md5(args); // получаем правильный хеш c помощью функции md5
+        let objectInCache = cache.find((item) => item.hash === hash); // ищем элемент, хеш которого равен нашему хешу
+        if (objectInCache) { // если элемент найден
+            console.log("Из кэша: " + objectInCache.value); // индекс нам известен, по индексу в массиве лежит объект, как получить нужное значение?
+            return "Из кэша: " + objectInCache.value;
+        }
+
+        let result = func(...args); // в кеше результата нет — придётся считать
+        cache.push({
+            "hash": hash,
+            "value": result
+        }); // добавляем элемент с правильной структурой
+        if (cache.length > 5) {
+            cache.shift(); // если слишком много элементов в кеше, надо удалить самый старый (первый) 
+        }
+        console.log("Вычисляем: " + result);
+        return "Вычисляем: " + result;
+    }
+    return wrapper;
 }
 
 //Задача № 2
 function debounceDecoratorNew(func, delay) {
-  
+    let timeoutId = null;
+    let isTrott = false;
+
+    function wrapper(...args) {
+        clearTimeout(timeoutId);
+        timeoutId = setTimeout(() => {
+            func(args);
+            wrapper.count++;
+        }, delay);
+        if (!isTrott) {
+            func(...args);
+            wrapper.count++;
+            isTrott = true;
+        }
+        wrapper.allCount++;
+    }
+    wrapper.count = 0;
+    wrapper.allCount = 0;
+    return wrapper;
 }
